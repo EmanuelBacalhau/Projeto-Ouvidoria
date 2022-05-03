1f34cf7d54532949cbbb5cd1990b15c038e74647
CARLOS EMANUEL BATISTA BACALHAU <carlosemanuelbatistabacalhau@gmail.com>
2022-05-03 00:18:50 -0300

    ouvidoria.v3
    Código de uma ouvidoria feita em Python --> Funções comentadas, uso de def e classes

--- Ouvidoria/OUVIDORIA/INTERFACE/__init__.py
+++ Ouvidoria/OUVIDORIA/INTERFACE/__init__.py
@@ -1,35 +1,30 @@
-def leiaint(txt):
+def leiaint(txt): # função criada para ler um número inteiro e validar (tratar erro)
     while True:
-        try:
+        try: # tentar ler o código 
             n = int(input(txt))
-        except (ValueError ,TypeError):
-            print('\033[31mErro: Insira um número válido\033[m')
-            continue
-        except (KeyboardInterrupt):
-            print('\033[31mErro: Insira um número válido\033[m')
-        else:
+        except (ValueError ,TypeError, KeyboardInterrupt): # caso não for o que foi pedido 
+            print('\033[31mErro: Insira um número válido\033[m') #imprime
+        else: # se der certo código contniua
             return n
 
-def leianome(txt):
+def leianome(txt): # função criada para ler str e validar (tratar erro)
     while True:
-        try:
+        try: # tentar ler o código 
             nome = str(input(txt))
-        except (ValueError, TypeError):
-            print('\033[31mErro: Insira um nome válido\033[m')
-        except (KeyboardInterrupt):
-            print('\033[31mErro: Insira um nome válido\033[m')
-        else:
+        except (ValueError, TypeError, KeyboardInterrupt): # caso não for o que foi pedido 
+            print('\033[31mErro: Insira um nome válido\033[m') #imprime
+        else: # se der certo código contniua
             return nome
 
-def linha(tamanho=100):
+def linha(tamanho=100): # função parar imprimir vários '='
     return '=' * tamanho
 
-def cabecalho(txt):
+def cabecalho(txt): # função para criar um nome e por no meio e entre linhas
     print(linha())
-    print(txt.center(100))
+    print(txt.center(100)) # 'center' == meio
     print(linha())
 
-def menu(lista):
+def menu(lista): # função parar formatar menu no código principal e ler sua opção
     c = 1
     for item in lista:
         print(f'\033[33m{c})\033[m \033[34m{item}\033[m')
--- Ouvidoria/sistema.py
+++ Ouvidoria/sistema.py
@@ -1,31 +1,31 @@
-from OUVIDORIA.INTERFACE import *
-from OUVIDORIA.OPCAO import *
-cabecalho('OUVIDORIA ABC')
+from OUVIDORIA.INTERFACE import * #Importar funções  
+from OUVIDORIA.OPCAO import * #Importar funções
+cabecalho('OUVIDORIA ABC') #'cabecalho' função importada da ouvidoria interface
 while True:
-    ops = OpcsEscondidos()
-    resp = menu(['Listar todas manifestações', 'Listar todas sugestões', 'Listar todas reclamações',
+    ops = OpcsEscondidos() # classe importada de OPCAO
+    resp = menu(['Listar todas manifestações', 'Listar todas sugestões', 'Listar todas reclamações', #menu formatado 
     'Listar todos elogios', 'Criar nova manifestação', 'Pesquisar manifestação pelo protocolo', 'Sair'])
-    if resp == 1:
+    if resp == 1: #1 listar todas manifestações
       cabecalho('Manifestação: ')
-      ops.opc1(manifestos) 
-    elif resp == 2:
+      ops.opc1(manifestos) #classe e função importada de OPCAO
+    elif resp == 2: #2 listar todas sugestões
       cabecalho('Sugestões: ')
       ops.opc2(manifestos)
-    elif resp == 3:
+    elif resp == 3: #3 listar todas reclamações
       cabecalho('Reclamações:')
       ops.opc3(manifestos)
-    elif resp == 4:
+    elif resp == 4: # listar todos elogios
       cabecalho('Elogios: ')
       ops.opc4(manifestos)
-    elif resp == 5:
+    elif resp == 5: # Criar nova manifestação
       cabecalho('Novo Manifesto: ')
       ops.opc5(tipos)
-    elif resp == 6:
+    elif resp == 6: # Procurar manifestação por número 
       cabecalho('Protocolos: ')
       ops.opc6(manifestos)
-    elif resp == 7:
+    elif resp == 7: # Sair
       cabecalho('Saindo... Até logo!')
       break
-    else:
+    else: # Caso digite algum número maior 7 ou menor que 1 == erro
       print('\033[31mErro: Digite uma opção válida!\033[m')
       print(linha())
